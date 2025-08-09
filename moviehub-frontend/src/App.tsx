94f62ee0af37d5f9f3545cc8c9f95598c726845c
Theeo04 <thgheorghe@gmail.com>
2025-08-09 19:25:57 +0300

    Restructure the code

--- moviehub-frontend/src/App.tsx
+++ moviehub-frontend/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <h1>Hello World!</h1>
       <main className="container mx-auto px-4 py-6 lg:pr-72">
         <CarouselMovie />
       </main>
